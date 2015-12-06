83f8232fea094e285b7515804179f636b06253bc
Maxim G <hexwer@gmail.com>
2015-12-07 02:14:50 +0300

    add topic chooser stub, high five recyclerview

--- app/src/main/java/doit/study/droid/MainActivity.java
+++ app/src/main/java/doit/study/droid/MainActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 
@@ -14,6 +15,7 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
     // Shia LaBeouf - Just Do it! (Auto-tuned)
     private final String URL = "http://www.youtube.com/watch?v=gJscrxxl_Bg";
+    @SuppressWarnings("unused")
     private final String LOG_TAG = "NSA " + getClass().getName();
 
     @Override
@@ -22,14 +24,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void doitButton(View v){
-        Intent intent = new Intent(MainActivity.this, QuestionsActivity.class);
-        startActivity(intent);
-    }
-
     public void motivationButton(View v){
         Intent motivIntent = new Intent(Intent.ACTION_VIEW);
         motivIntent.setData(Uri.parse(URL));
         startActivity(motivIntent);
+    }
+
+    public void setTopicButton(View v){
+        Intent intent = new Intent(MainActivity.this, TopicsActivity.class);
+        startActivity(intent);
+    }
+
+    public void doitButton(View v){
+        Intent intent = new Intent(MainActivity.this, QuestionsActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/doit/study/droid/TopicsActivity.java
@@ -0,0 +1,58 @@
+package doit.study.droid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class TopicsActivity extends AppCompatActivity {
+    @SuppressWarnings("unused")
+    private final String LOG_TAG = "NSA " + getClass().getName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.topics_layout);
+        RecyclerView rv = (RecyclerView) findViewById(R.id.topics_view);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        GlobalData gd = (GlobalData) getApplication();
+        rv.setAdapter(new TopicAdapter(gd.getQuizData()));
+    }
+
+    private static class TopicAdapter extends RecyclerView.Adapter<TopicAdapter.TopicViewHolder>{
+        QuizData mQuizData;
+        public TopicAdapter(QuizData quizData){
+            mQuizData = quizData;
+        }
+
+        public static class TopicViewHolder extends RecyclerView.ViewHolder{
+            TextView topic;
+            public TopicViewHolder(View itemView) {
+                super(itemView);
+                topic = (TextView)itemView.findViewById(R.id.topic_name);
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return mQuizData.size();
+        }
+
+        @Override
+        public TopicViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.topic_item, parent, false);
+            return new TopicViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(TopicViewHolder holder, int position) {
+            holder.topic.setText(mQuizData.getById(position).getText());
+        }
+    }
+}
