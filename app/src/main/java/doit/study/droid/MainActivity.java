b34f9b9790d1a7d6e8618126917844a92377e87e
Maxim G <hexwer@gmail.com>
2015-12-07 11:16:32 +0300

    optimize imports

--- app/src/main/java/doit/study/droid/MainActivity.java
+++ app/src/main/java/doit/study/droid/MainActivity.java
@@ -4,9 +4,7 @@ package doit.study.droid;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 
 
--- app/src/main/java/doit/study/droid/QuestionsActivity.java
+++ app/src/main/java/doit/study/droid/QuestionsActivity.java
@@ -1,7 +1,6 @@
 package doit.study.droid;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
