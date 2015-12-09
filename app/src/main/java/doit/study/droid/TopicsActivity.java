7459acf241fdabf4758d4cceef0901a63c3e00c7
Maxim G <hexwer@gmail.com>
2015-12-09 21:39:58 +0300

    resolve #29: add divider between items in RecyclerView

--- app/src/main/java/doit/study/droid/TopicsActivity.java
+++ app/src/main/java/doit/study/droid/TopicsActivity.java
@@ -1,6 +1,11 @@
 package doit.study.droid;
 
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -22,6 +27,7 @@ public class TopicsActivity extends AppCompatActivity {
         setContentView(R.layout.topics_layout);
         RecyclerView rv = (RecyclerView) findViewById(R.id.topics_view);
         rv.setLayoutManager(new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL));
+        rv.addItemDecoration(new DividerItemDecoration(this, R.drawable.divider));
         GlobalData gd = (GlobalData) getApplication();
         rv.setAdapter(new TopicAdapter(gd.getQuizData()));
     }
@@ -54,6 +60,48 @@ public class TopicsActivity extends AppCompatActivity {
         @Override
         public void onBindViewHolder(TopicViewHolder holder, int position) {
             holder.topic.setText(mQuizData.getById(position).getText());
+        }
+    }
+
+    private static class DividerItemDecoration extends RecyclerView.ItemDecoration {
+
+        private final int[] ATTRS = new int[]{android.R.attr.listDivider};
+
+        private Drawable mDivider;
+
+        /**
+         * Default divider will be used
+         */
+        public DividerItemDecoration(Context context) {
+            final TypedArray styledAttributes = context.obtainStyledAttributes(ATTRS);
+            mDivider = styledAttributes.getDrawable(0);
+            styledAttributes.recycle();
+        }
+
+        /**
+         * Custom divider will be used
+         */
+        public DividerItemDecoration(Context context, int resId) {
+            mDivider = ContextCompat.getDrawable(context, resId);
+        }
+
+        @Override
+        public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+            int left = parent.getPaddingLeft();
+            int right = parent.getWidth() - parent.getPaddingRight();
+
+            int childCount = parent.getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                View child = parent.getChildAt(i);
+
+                RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+                int top = child.getBottom() + params.bottomMargin;
+                int bottom = top + mDivider.getIntrinsicHeight();
+
+                mDivider.setBounds(left, top, right, bottom);
+                mDivider.draw(c);
+            }
         }
     }
 }
