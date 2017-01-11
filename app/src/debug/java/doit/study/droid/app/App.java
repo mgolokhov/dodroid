381219bfcc00828635ccc87a385608c379977e7d
Maxim G <hexwer@gmail.com>
2017-01-11 13:18:30 +0300

    add logger for release build powered by crashlytics

--- app/src/debug/java/doit/study/droid/app/App.java
+++ app/src/debug/java/doit/study/droid/app/App.java
@@ -7,12 +7,14 @@ public class App extends BaseApp {
     @Override
     public void onCreate() {
         super.onCreate();
-        Timber.plant(new Timber.DebugTree(){
-            @Override
-            protected String createStackElementTag(StackTraceElement element) {
-                return "NSA:" + super.createStackElementTag(element) + ":" + element.getLineNumber();
-            }
-        });
+        Timber.plant(new MyDebugTree());
         Timber.d("Debug mode with logging");
+    }
+
+    private static class MyDebugTree extends Timber.DebugTree{
+        @Override
+        protected String createStackElementTag(StackTraceElement element) {
+            return "NSA:" + super.createStackElementTag(element) + ":" + element.getLineNumber();
+        }
     }
 }
