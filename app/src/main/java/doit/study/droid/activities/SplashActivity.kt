de9f441787e2e2ccc5491b939db6ff3f61b2e112
Maxim Golokhov <hexwer@gmail.com>
2019-10-13 19:03:13 +0300

    implement network requests with coroutines

--- app/src/main/java/doit/study/droid/activities/SplashActivity.kt
+++ app/src/main/java/doit/study/droid/activities/SplashActivity.kt
@@ -4,67 +4,49 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
-
 import androidx.appcompat.app.AppCompatActivity
-
 import doit.study.droid.R
 import doit.study.droid.app.App
-import doit.study.droid.data.Configuration
 import doit.study.droid.data.Question
 import doit.study.droid.data.QuizDBHelper
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import timber.log.Timber
 
 class SplashActivity : AppCompatActivity() {
-    private val sharedPref: SharedPreferences by lazy {getSharedPreferences(PREF_NAME, MODE_PRIVATE)}
-
-    val handleQuestionsResponse = object : Callback<List<Question>> {
-        override fun onResponse(call: Call<List<Question>>, response: Response<List<Question>>) {
-            try {
-                populateDB(response)
-                navigateToTopicChooser()
-            } catch (e: Exception) {
-                handleErrCannotLoadQuiz(e)
-            }
-
-        }
-
-        override fun onFailure(call: Call<List<Question>>, t: Throwable) {
-            handleErrCannotLoadQuiz(t)
-        }
-
+    private val sharedPref: SharedPreferences by lazy {
+        getSharedPreferences(PREVIOUS_CONTENT_VERSION, MODE_PRIVATE)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val quizDataClient = (application as App).quizService
 
-        quizDataClient.configuration.enqueue(object : Callback<Configuration> {
-            override fun onFailure(call: Call<Configuration>, t: Throwable) {
-                handleErrCannotLoadQuiz(t)
-            }
-
-            override fun onResponse(call: Call<Configuration>, response: Response<Configuration>) {
-                try {
-                    response.body()?.contentVersion?.let {
-                        val previousVersion = sharedPref.getInt(PREF_NAME, 0)
-                        if (it > previousVersion) {
-                            quizDataClient.questions.enqueue(handleQuestionsResponse)
-                            sharedPref.edit().putInt(PREF_NAME, it).apply()
-                        } else {
-                            navigateToTopicChooser()
-                        }
+        GlobalScope.launch(Dispatchers.Main) {
+            try {
+                withContext(Dispatchers.IO) {
+                    val config = quizDataClient.configuration()
+                    if (getPreviousVersion() < config.contentVersion) {
+                        populateDB(quizDataClient.questions())
+                        savePreviousVersion(config.contentVersion)
                     }
-                } catch (e: Exception) {
-                    handleErrCannotLoadQuiz(e)
                 }
+            } catch (e: Exception) {
+                handleErrCannotLoadQuiz(e)
+            } finally {
+                navigateToTopicChooser()
             }
-
-        })
+        }
     }
+
+    private fun getPreviousVersion() = sharedPref.getInt(PREVIOUS_CONTENT_VERSION, 0)
+
+    private fun savePreviousVersion(version: Int) = sharedPref
+            .edit()
+            .putInt(PREVIOUS_CONTENT_VERSION, version)
+            .apply()
 
     private fun handleErrCannotLoadQuiz(t: Throwable) {
         Toast.makeText(this, R.string.failed_to_load_quiz, Toast.LENGTH_SHORT).show()
@@ -78,13 +60,13 @@ class SplashActivity : AppCompatActivity() {
         finish()
     }
 
-    private fun populateDB(response: Response<List<Question>>){
+    private fun populateDB(questions: List<Question>){
         val helper = QuizDBHelper(baseContext)
-        helper.insertFromFile(response.body(), helper.writableDatabase)
+        helper.insertFromFile(questions, helper.writableDatabase)
         helper.close()
     }
 
     companion object {
-        private const val PREF_NAME = "SplashActivity.content.version"
+        private const val PREVIOUS_CONTENT_VERSION = "SplashActivity.content.version"
     }
 }
