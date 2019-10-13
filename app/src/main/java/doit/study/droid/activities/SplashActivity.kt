249d19d365f3c91883808cc13fc3e01744824891
Maxim Golokhov <hexwer@gmail.com>
2019-10-13 18:02:41 +0300

    add config endpoint to check version for db

--- app/src/main/java/doit/study/droid/activities/SplashActivity.kt
+++ app/src/main/java/doit/study/droid/activities/SplashActivity.kt
@@ -1,6 +1,7 @@
 package doit.study.droid.activities
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
 
@@ -8,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 import doit.study.droid.R
 import doit.study.droid.app.App
+import doit.study.droid.data.Configuration
 import doit.study.droid.data.Question
 import doit.study.droid.data.QuizDBHelper
 import retrofit2.Call
@@ -16,39 +18,73 @@ import retrofit2.Response
 import timber.log.Timber
 
 class SplashActivity : AppCompatActivity() {
+    private val sharedPref: SharedPreferences by lazy {getSharedPreferences(PREF_NAME, MODE_PRIVATE)}
+
+    val handleQuestionsResponse = object : Callback<List<Question>> {
+        override fun onResponse(call: Call<List<Question>>, response: Response<List<Question>>) {
+            try {
+                populateDB(response)
+                navigateToTopicChooser()
+            } catch (e: Exception) {
+                handleErrCannotLoadQuiz(e)
+            }
+
+        }
+
+        override fun onFailure(call: Call<List<Question>>, t: Throwable) {
+            handleErrCannotLoadQuiz(t)
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val quizDataClient = (application as App).quizService
 
-        (application as App).quizService.questions.enqueue(object : Callback<List<Question>> {
-            override fun onResponse(call: Call<List<Question>>, response: Response<List<Question>>) {
-                try {
-                    val helper = QuizDBHelper(baseContext)
-                    helper.insertFromFile(response.body(), helper.writableDatabase)
-                    helper.close()
-
-                    val intent = Intent(this@SplashActivity, TopicsChooserActivity::class.java)
-                    startActivity(intent)
-                    finish()
-                } catch (e: Exception) {
-                    handleErrCannotLoadQuiz(e)
-                }
-
-            }
-
-            override fun onFailure(call: Call<List<Question>>, t: Throwable) {
+        quizDataClient.configuration.enqueue(object : Callback<Configuration> {
+            override fun onFailure(call: Call<Configuration>, t: Throwable) {
                 handleErrCannotLoadQuiz(t)
             }
 
-
-            private fun handleErrCannotLoadQuiz(t: Throwable) {
-                Toast.makeText(this@SplashActivity, R.string.failed_to_load_quiz, Toast.LENGTH_SHORT).show()
-                Timber.e(t)
-                finish()
+            override fun onResponse(call: Call<Configuration>, response: Response<Configuration>) {
+                try {
+                    response.body()?.contentVersion?.let {
+                        val previousVersion = sharedPref.getInt(PREF_NAME, 0)
+                        if (it > previousVersion) {
+                            quizDataClient.questions.enqueue(handleQuestionsResponse)
+                            sharedPref.edit().putInt(PREF_NAME, it).apply()
+                        } else {
+                            navigateToTopicChooser()
+                        }
+                    }
+                } catch (e: Exception) {
+                    handleErrCannotLoadQuiz(e)
+                }
             }
+
         })
+    }
 
+    private fun handleErrCannotLoadQuiz(t: Throwable) {
+        Toast.makeText(this, R.string.failed_to_load_quiz, Toast.LENGTH_SHORT).show()
+        Timber.e(t)
+        finish()
+    }
 
+    private fun navigateToTopicChooser(){
+        val intent = Intent(this, TopicsChooserActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun populateDB(response: Response<List<Question>>){
+        val helper = QuizDBHelper(baseContext)
+        helper.insertFromFile(response.body(), helper.writableDatabase)
+        helper.close()
+    }
+
+    companion object {
+        private const val PREF_NAME = "SplashActivity.content.version"
     }
 }
--- app/src/main/java/doit/study/droid/data/QuizDataClient.java
+++ app/src/main/java/doit/study/droid/data/QuizDataClient.java
@@ -8,4 +8,6 @@ import retrofit2.http.GET;
 public interface QuizDataClient {
     @GET("quiz.json?cache_stub=drop")
     Call<List<Question>> getQuestions();
+    @GET("config?cache_stub=drop")
+    Call<Configuration> getConfiguration();
 }
