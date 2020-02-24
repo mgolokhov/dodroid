0a2c95c12f0f8c1f50d027fbbcd9a47952a7191f
Maxim Golokhov <hexwer@gmail.com>
2020-02-24 12:43:29 +0300

    a bit cleanup to satisfy lint

--- app/src/main/java/doit/study/droid/common/ViewModelFactory.kt
+++ app/src/main/java/doit/study/droid/common/ViewModelFactory.kt
@@ -6,7 +6,7 @@ import javax.inject.Inject
 import javax.inject.Provider
 import javax.inject.Singleton
 
-
+@Suppress("UNCHECKED_CAST")
 @Singleton
 class ViewModelFactory @Inject constructor(
         private val viewModels: MutableMap<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
--- app/src/main/java/doit/study/droid/data/local/entity/QuestionTagJoin.kt
+++ app/src/main/java/doit/study/droid/data/local/entity/QuestionTagJoin.kt
@@ -2,20 +2,27 @@ package doit.study.droid.data.local.entity
 
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(foreignKeys = [
-    ForeignKey(
-            entity = Question::class,
-            parentColumns = ["id"],
-            childColumns = ["questionId"]
-    ),
-    ForeignKey(
-            entity = Tag::class,
-            parentColumns = ["id"],
-            childColumns = ["tagId"]
-    )
-])
+@Entity(
+        foreignKeys = [
+            ForeignKey(
+                    entity = Question::class,
+                    parentColumns = ["id"],
+                    childColumns = ["questionId"]
+            ),
+            ForeignKey(
+                    entity = Tag::class,
+                    parentColumns = ["id"],
+                    childColumns = ["tagId"]
+            )
+        ],
+        indices = [
+            Index("questionId"),
+            Index("tagId")
+        ]
+)
 data class QuestionTagJoin(
         @PrimaryKey(autoGenerate = true)
         val id: Int = 0,
